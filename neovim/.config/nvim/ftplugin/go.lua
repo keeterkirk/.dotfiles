bef7b03e958e714e7768b96b068eb2f969049431
Keith Thibodeaux <keith@railscoder.net>
2025-05-20 10:32:13 -0400

    vim: more go support

--- neovim/.config/nvim/ftplugin/go.lua
+++ neovim/.config/nvim/ftplugin/go.lua
@@ -7,6 +7,8 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.copyindent = true
 vim.opt_local.preserveindent = true
 
+buffer_map(0, 'n', '<leader>.', ':lua require("open_alt").open()<CR>', { noremap = true, desc = 'open alt' })
+
 buffer_map(0, 'n', '<leader>rf', ":lua _G.tests.go.run_file()<CR>", { noremap = true, desc = 'current file' })
 buffer_map(0, 'n', '<leader>ra', ':lua _G.tests.go.run_all()<CR>', { noremap = true, desc = 'all files' })
 buffer_map(0, 'n', '<leader>rl', ":lua _G.tests.run_last()<CR>", { noremap = true, desc = 'last test' })
