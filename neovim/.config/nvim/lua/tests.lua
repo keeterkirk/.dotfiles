5e537496c240e8c75e34d8e6693db2f15fdc0867
Kirk Keeter <kirk.keeter@prizepicks.com>
2025-10-13 09:53:46 -0500

    Updates from Keithie Poo

--- neovim/.config/nvim/lua/tests.lua
+++ neovim/.config/nvim/lua/tests.lua
@@ -1,3 +1,13 @@
+local function get_line_content(n)
+  local l = vim.api.nvim_buf_get_lines(0, n - 1, n, false)
+  return l[1] or ""
+end
+
+local function get_line_number()
+  local line, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  return line
+end
+
 local function get_filename()
   local cwd = vim.fn.getcwd()
   local full_path = vim.fn.expand('%:p')
@@ -6,8 +16,16 @@ local function get_filename()
   return relative_path
 end
 
+local function ensure_spec(filename)
+  if filename:find("spec", 1, true) then
+    return filename
+  else
+    return require('open_alt').get_alternate_filename(filename)
+  end
+end
+
 local function remove_until(original_string, until_string)
-  local start_position, end_position = original_string:find(until_string)
+  local start_position, _ = original_string:find(until_string)
 
   if start_position then
     return original_string:sub(start_position)
@@ -16,9 +34,23 @@ local function remove_until(original_string, until_string)
   end
 end
 
-local function get_line_number()
-  local line, column = unpack(vim.api.nvim_win_get_cursor(0))
-  return line
+local function find_go_test_name()
+  local cur_line = get_line_number()
+  local line_text = get_line_content(cur_line)
+  local on_name = line_text:match("^%s*func%s+(Test[%w_]+)%s*%(")
+  if on_name then
+    return on_name
+  end
+
+  for line_number = cur_line, 1, -1 do
+    local txt = get_line_content(line_number)
+    local name = txt:match("^%s*func%s+(Test[%w_]+)%s*%(")
+    if name then
+      return name
+    end
+  end
+
+  return nil
 end
 
 local go = {
@@ -29,12 +61,31 @@ local go = {
     local filename = get_filename()
 
     local directory = filename:match("(.*/)")
-
-    if directory ~= "./" then
+    if not directory then
+      directory = "..."
+    elseif directory ~= "./" then
       directory = directory:sub(1, #directory - 1)
     end
 
-    vim.cmd('VimuxRunCommand("go test ./' .. directory .. '")')
+    vim.cmd('VimuxRunCommand("go test -v ./' .. directory .. '")')
+  end,
+  run_nearest = function()
+    local filename = get_filename()
+
+    local directory = filename:match("(.*/)")
+    if not directory then
+      directory = "..."
+    elseif directory ~= "./" then
+      directory = directory:sub(1, #directory - 1)
+    end
+
+    local test_name = find_go_test_name()
+
+    if test_name then
+      vim.cmd('VimuxRunCommand("go test -v ./' .. directory .. ' -run ' .. test_name .. '")')
+    else
+      vim.cmd('VimuxRunCommand("go test -v ./' .. directory .. '")')
+    end
   end,
 }
 
@@ -53,12 +104,12 @@ local rspec = {
     vim.cmd("VimuxRunCommand('bundle exec rspec')")
   end,
   run_file = function()
-    local filename = remove_until(get_filename(), "spec")
-    vim.cmd('VimuxRunCommand("bundle exec rspec ./' .. filename .. '")')
+    local filename = remove_until(ensure_spec(get_filename()), "spec")
+    vim.cmd('VimuxRunCommand("bundle exec rspec -f doc ./' .. filename .. '")')
   end,
   run_nearest = function()
-    local filename = remove_until(get_filename(), "spec")
-    vim.cmd('VimuxRunCommand("bundle exec rspec ./' .. filename .. ':' .. get_line_number() .. '")')
+    local filename = remove_until(ensure_spec(get_filename()), "spec")
+    vim.cmd('VimuxRunCommand("bundle exec rspec -f doc ./' .. filename .. ':' .. get_line_number() .. '")')
   end,
 }
 
